6226f1d023c4140a86aea40d693b907e80d45041
Michal Mirowski <mirowskima@gmail.com>
2026-02-04 14:58:40 +0100

    chore: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import eslintPluginPlaywright from 'eslint-plugin-playwright';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  { ignores: ['package-lock.json', 'playwright-report/**', 'test-results/**'] },
+  {
+    files: ['**/*.ts'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      'no-console': 'error',
+    },
+  },
+  eslintPluginPlaywright.configs['flat/recommended'],
+  {
+    rules: {
+      'playwright/no-nested-step': 'off',
+    },
+    settings: {
+      playwright: {
+        globalAliases: {
+          test: ['setup'],
+        },
+      },
+    },
+  },
+  eslintPluginPrettierRecommended,
+]);
